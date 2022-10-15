8abc0761a884c30b1d5f50a8f735be2569f15c9a
Silent <zdanio95@gmail.com>
2022-10-15 17:49:53 +0200

    Un-hardcoded co-driver language

--- source/SilentPatchCMR3.cpp
+++ source/SilentPatchCMR3.cpp
@@ -46,6 +46,27 @@ namespace Localization
 
 		return "";
 	}
+
+	uint32_t (*orgGetCurrentLanguageID)();
+
+	uint8_t* gCoDriverLanguage;
+	uint32_t GetCoDriverLanguage()
+	{
+		return *gCoDriverLanguage;
+	}
+
+	void SetCoDriverLanguage(uint32_t language)
+	{
+		*gCoDriverLanguage = static_cast<int8_t>(language);
+	}
+
+	static void (*orgSetUnknown)(int a1);
+	static void setUnknown_AndCoDriverLanguage(int a1)
+	{
+		SetCoDriverLanguage(orgGetCurrentLanguageID());
+
+		orgSetUnknown(a1);
+	}
 }
 
 void OnInitializeHook()
@@ -67,6 +88,35 @@ void OnInitializeHook()
 		gActiveLanguage = *get_pattern<LangFile**>("89 0D ? ? ? ? B8 ? ? ? ? 5E", 2);
 		InjectHook(get_localized_string, GetLocalizedString_BoundsCheck, PATCH_JUMP);
 
+	}
+	TXN_CATCH();
+
+
+	// Un-hardcoded co-driver language
+	// Patch will probably fail pattern matches on set_defaults when used on the English executable
+	try
+	{
+		using namespace Localization;
+
+		auto get_current_language_id = static_cast<decltype(orgGetCurrentLanguageID)>(get_pattern("6A 45 E8 ? ? ? ? C3"));
+
+		auto get_codriver_language = ReadCallFrom(get_pattern("E8 ? ? ? ? 83 F8 03 77 40"));
+		auto set_defaults = pattern("A2 ? ? ? ? C6 05 ? ? ? ? 01 88 1D").get_one();
+		auto unk_on_main_menu_start = get_pattern("75 ? 6A 02 E8 ? ? ? ? 81 C4", 4);
+
+		orgGetCurrentLanguageID = get_current_language_id;
+
+		gCoDriverLanguage = *set_defaults.get<uint8_t*>(5 + 2);
+		InjectHook(get_codriver_language, GetCoDriverLanguage, PATCH_JUMP);
+
+		//  mov bCoDriverLanguage, 1 -> mov bCoDriverLanguage, al
+		Patch(set_defaults.get<void>(5), { 0x90, 0xA2 });
+		Patch(set_defaults.get<void>(5 + 2), gCoDriverLanguage);
+		Nop(set_defaults.get<void>(5 + 6), 1);
+
+		// Unknown, called once as main menu starts
+		ReadCall(unk_on_main_menu_start, orgSetUnknown);
+		InjectHook(unk_on_main_menu_start, setUnknown_AndCoDriverLanguage);
 	}
 	TXN_CATCH();
 }
