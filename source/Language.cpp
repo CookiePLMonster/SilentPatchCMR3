42108f09b2f998e3c779351df5d3201c8943465f
Silent <zdanio95@gmail.com>
2023-01-02 18:11:06 +0100

    Final translation lines with hardcoded Polish fallbacks

--- source/Language.cpp
+++ source/Language.cpp
@@ -44,38 +44,74 @@ static std::map<uint32_t, const char*> GetOverrideStrings()
 static std::map<uint32_t, const char*> GetNewStrings()
 {
 	std::map<uint32_t, const char*> result = {
-		{ 994, "NA" },
-		{ 1004, "RETURN" }, { 1005, "ESC" }, { 1006, "BACKSPC" }, { 1007, "TAB" },
-		{ 1008, "F1" }, { 1009, "F2" }, { 1010, "F3" }, { 1011, "F4" },
-		{ 1012, "F5" }, { 1013, "F6" }, { 1014, "F7" }, { 1015, "F8" },
-		{ 1016, "F9" }, { 1017, "F10" }, { 1018, "F11" }, { 1019, "F12" },
-		{ 1020, "PRINTSCRN" }, { 1021, "SCROLLLOCK" }, { 1022, "PAUSE" }, { 1023, "INSERT" },
-		{ 1024, "HOME" }, { 1025, "PGUP" }, { 1026, "DEL" }, { 1027, "END" },
-		{ 1028, "PGDN" }, { 1029, "RIGHT" }, { 1030, "LEFT" }, { 1031, "UP" },
-		{ 1032, "DOWN" }, { 1033, "[?]" },
-
-		// TODO: Translate those to Polish if loc pack is not installed
-		{ RETURN_TO_CENTRE, "RETURN TO CENTRE" },
-
 		{ LANGUAGE_POLISH, "POLSKI" },
 		{ LANGUAGE_CZECH, "\xC3" "ESKY" },
 
-		{ CODRIVER_POLISH_A, "POLISH (J. KULIG)" },
-		{ CODRIVER_POLISH_B, "POLISH (J. WITUCH)" },
-		{ CODRIVER_CZECH, "CZECH" },
+		{ 1008, "F1" }, { 1009, "F2" }, { 1010, "F3" }, { 1011, "F4" },
+		{ 1012, "F5" }, { 1013, "F6" }, { 1014, "F7" }, { 1015, "F8" },
+		{ 1016, "F9" }, { 1017, "F10" }, { 1018, "F11" }, { 1019, "F12" },
 
-		{ DISPLAY_MODE, "DISPLAY MODE" },
-		{ FULLSCREEN, "FULLSCREEN" },
-		{ WINDOWED, "WINDOWED" },
-		{ BORDERLESS, "BORDERLESS" },
-		{ REFRESH_RATE, "REFRESH RATE" },
-		{ VSYNC, "VSYNC" },
-		{ ANISOTROPIC, "ANISOTROPIC FILTERING" },
-
-		{ TACHOMETER, "TACHOMETER" },
-		{ EXTERIOR_FOV, "FOV (CHASE)" },
-		{ INTERIOR_FOV, "FOV (INTERIOR)" },
+		{ 1033, "[?]" },
 	};
+
+	if (Version::IsPolish())
+	{
+		result.merge(std::map<uint32_t, const char*> {
+			{ 994, "ND" },
+			{ 1004, "ENTER" }, { 1005, "ESC" }, { 1006, "BACKSPACE" }, { 1007, "TAB" },
+			{ 1020, "PRINTSCREEN" }, { 1021, "SCROLLLOCK" }, { 1022, "PAUZA" }, { 1023, "INSERT" },
+			{ 1024, "HOME" }, { 1025, "PGUP" }, { 1026, "DELETE" }, { 1027, "END" },
+			{ 1028, "PGDN" }, { 1029, "PRAWO" }, { 1030, "LEWO" }, { 1031, "G\xD3RA" },
+			{ 1032, "D\xD3\xA3" },
+
+			{ RETURN_TO_CENTRE, "POWR\xD3T DO POZ. NEUTRALNEJ" },
+
+			{ CODRIVER_POLISH_A, "JANUSZ KULIG" },
+			{ CODRIVER_POLISH_B, "JANUSZ WITUCH" },
+			{ CODRIVER_CZECH, "CZESKI" },
+
+			{ DISPLAY_MODE, "TRYB WY\x8CWIETLANIA" },
+			{ FULLSCREEN, "PE\xA3NY EKRAN" },
+			{ WINDOWED, "W OKNIE" },
+			{ BORDERLESS, "BEZ RAMKI" },
+			{ REFRESH_RATE, "CZ\xCAST. OD\x8CWIE\xAF" "ANIA" },
+			{ VSYNC, "SYNCHRONIZACJA PION." },
+			{ ANISOTROPIC, "FILTROWANIE ANIZOTROPOWE" },
+
+			{ TACHOMETER, "OBROTOMIERZ" },
+			{ EXTERIOR_FOV, "POLE WIDZENIA (ZEWN.)" },
+			{ INTERIOR_FOV, "POLE WIDZENIA (WEWN.)" },
+		});
+	}
+	else
+	{
+		result.merge(std::map<uint32_t, const char*> {
+			{ 994, "NA" },
+			{ 1004, "RETURN" }, { 1005, "ESC" }, { 1006, "BACKSPC" }, { 1007, "TAB" },
+			{ 1020, "PRINTSCRN" }, { 1021, "SCROLLLOCK" }, { 1022, "PAUSE" }, { 1023, "INSERT" },
+			{ 1024, "HOME" }, { 1025, "PGUP" }, { 1026, "DEL" }, { 1027, "END" },
+			{ 1028, "PGDN" }, { 1029, "RIGHT" }, { 1030, "LEFT" }, { 1031, "UP" },
+			{ 1032, "DOWN" },
+
+			{ RETURN_TO_CENTRE, "RETURN TO CENTRE" },
+
+			{ CODRIVER_POLISH_A, "POLISH (J. KULIG)" },
+			{ CODRIVER_POLISH_B, "POLISH (J. WITUCH)" },
+			{ CODRIVER_CZECH, "CZECH" },
+
+			{ DISPLAY_MODE, "DISPLAY MODE" },
+			{ FULLSCREEN, "FULLSCREEN" },
+			{ WINDOWED, "WINDOWED" },
+			{ BORDERLESS, "BORDERLESS" },
+			{ REFRESH_RATE, "REFRESH RATE" },
+			{ VSYNC, "VSYNC" },
+			{ ANISOTROPIC, "ANISOTROPIC FILTERING" },
+
+			{ TACHOMETER, "TACHOMETER" },
+			{ EXTERIOR_FOV, "FIELD OF VIEW (CHASE)" },
+			{ INTERIOR_FOV, "FIELD OF VIEW (INTERIOR)" },
+		});
+	}
 
 	return result;
 }
