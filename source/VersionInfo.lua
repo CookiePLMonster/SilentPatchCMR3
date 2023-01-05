245fd5443185c1b2a45dbe3e17acf451544a06b0
Silent <zdanio95@gmail.com>
2023-01-05 19:04:00 +0100

    Update copyright years

--- source/VersionInfo.lua
+++ source/VersionInfo.lua
@@ -3,6 +3,6 @@ defines {
     "rsc_MinorVersion=0",
     "rsc_RevisionID=1",
     "rsc_BuildID=0",
-    "rsc_Copyright=\"2022\"",
+    "rsc_Copyright=\"2022-2023\"",
     "rsc_UpdateURL=\"https://github.com/CookiePLMonster/SilentPatchCMR3/releases\""
 }
